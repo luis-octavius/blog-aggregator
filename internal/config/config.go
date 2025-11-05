bfd675602f9213ad7b75c85bd634c9462885b72b
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-04 21:28:20 -0300

    feat(Config): add methods to create and read gatorconfig
    - Implement Config struct
    - Implement Read() that returns a new config struct
    - Implement configFilePath() to return the path of config file
    - Implement SetUser() and write() to change the user of config and write
      to the file

---
+++ internal/config/config.go
@@ -0,0 +1,67 @@
+package config 
+
+import (
+	"os"
+	"encoding/json"
+	"fmt"
+	"bytes"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	Db_url 						string `json:"db_url"`  
+	Current_user_name string `json:"current_user_name"`
+}
+
+func Read() Config {
+	configFilePath, err := getConfigFilePath()
+	fmt.Println("config file path: ", configFilePath)
+	if err != nil {
+		return Config{}
+	}
+
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		fmt.Printf("error opening config file: %v\n", err)
+		return Config{}
+	}
+
+	defer configFile.Close()
+	
+	data := make([]byte, 200)
+	_, err = configFile.Read(data)
+	if err != nil {
+		fmt.Printf("error reading the contents of config file: %v\n", err)
+	}
+
+	trimmedBytes := bytes.Trim(data, "\x00") // 
+
+	cfg := Config{
+		Db_url: "postgres://example", 
+		Current_user_name: "default",
+	} 
+
+	err = json.Unmarshal(trimmedBytes, &cfg) 
+	if err != nil {
+		fmt.Printf("error unmarshaling data from config file: %v\n", err)
+		return Config{}
+	}
+
+	return cfg
+}
+
+func (cfg Config) SetUser(currentUser string) error {
+	cfg.Current_user_name = currentUser 
+
+	err := write(cfg)
+	if err != nil {
+		fmt.Printf("error setting user: %v\n", err) 
+	}
+
+	return nil 
+}
+
+
+
+
---
+++ internal/config/utils.go
@@ -0,0 +1,47 @@
+package config 
+
+import (
+	"fmt"
+	"path"
+	"os"
+	"encoding/json"
+)
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("error getting the home path: %w", err)
+	}
+
+	configFilePath := path.Join(home, configFileName)
+	return configFilePath, nil 
+}
+
+func write(cfg Config) error {
+	jsonData, err := json.Marshal(cfg) 
+	if err != nil {
+		return fmt.Errorf("error marshaling data: %w", err)
+	}
+
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("error getting the config file path: %w", err) 
+	}
+
+	err = os.Remove(configFilePath) 
+	if err != nil {
+		return fmt.Errorf("error removing the config file path: %w", err)
+	}
+
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		return fmt.Errorf("error creating the config file path: %w", err)
+	}
+
+	_, err = file.Write(jsonData) 
+	if err != nil {
+		return fmt.Errorf("error writing to config file with json data: %w", err)
+	}
+	
+	return nil 
+}
