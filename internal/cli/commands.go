c4960a2230cfaae50007256fd6653ae5d20c09df
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-05 21:11:32 -0300

    feat(cli): add commands run and register, and Commands type
    - implement Commands struct (Commands)
    - implement Run to validate a command in cli
    - implement Register to save a new command in Commands struct map field

---
+++ internal/cli/commands.go
@@ -0,0 +1,29 @@
+package cli 
+
+import (
+	"fmt"
+	
+	"github.com/luis-octavius/blog-aggregator/internal/types"
+)
+
+type Commands struct {
+	Commands map[string]func(*types.State, Command) error 
+}
+
+func (c *Commands) Run(s *types.State, cmd Command) error {
+	command, ok := c.Commands[cmd.Name]
+	if !ok {
+		return fmt.Errorf("command not found")
+	}
+	
+	err := command(s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil 
+}
+
+func (c *Commands) Register(name string, f func(*types.State, Command) error) error {
+	c.Commands[name] = f 
+	return nil 
+}		
