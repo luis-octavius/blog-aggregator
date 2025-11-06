6a51d845dab24911625c934d898e98cd3b12e27d
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-05 21:14:17 -0300

    feat(main): set up a simple flow to test the following implementations
    - State, Command and Commands struct
    - validate args by running the program in cli
    - methods of Commands like Run and Register

--- main.go
+++ main.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/luis-octavius/blog-aggregator/internal/config"
+	"github.com/luis-octavius/blog-aggregator/internal/types"
+	"github.com/luis-octavius/blog-aggregator/internal/cli"
 )
 
 func main() {
-	cfg := config.Read()
-	cfg.SetUser("luis")
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Errorf("error creating config: %v", err)
+	}
 
-	cfg = config.Read()
+	state := types.State{
+		Config: &cfg,
+	} 	
+
+	commandsHandler := cli.Commands{
+		Commands: map[string]func(*types.State, cli.Command) error{},
+	}
+
+	commandsHandler.Register("login", cli.HandlerLogin)
+	
+	args := os.Args 
+
+	fmt.Println(args)
+
+	if len(args) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
+	}
+
+	cmd := cli.Command{
+		Name: args[1],
+		Args: args[2:],
+	}
+
+	err = commandsHandler.Run(&state, cmd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
